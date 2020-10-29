26fd71de3cf6cd78a58a6ccf661846373f0dd9af
Danny Noonan <dnoonan@cern.ch>
2020-10-29 16:11:27 -0500

    make duplicate of eport data with hex

---
+++ convertToHex.py
@@ -0,0 +1,20 @@
+import argparse
+import pandas as pd
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--input', dest='input', required=True, help='input dir file name')
+args = parser.parse_args()
+
+inputFile = args.input
+
+df = pd.read_csv(inputFile)
+
+vHex = np.vectorize(hex)
+
+cols = [f'ePortRxDataGroup_{i}' for i in range(12)]
+
+df[cols] = vHex(df[cols])
+
+df.to_csv(inputFile.replace('.csv','_HEX.csv'))
+

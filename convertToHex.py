f58d3943f2d57d7533e97f34502c0577aca53100
cmantill <criss.ms7@gmail.com>
2021-07-13 14:57:29 +0000

    general HEX conversion

--- convertToHex.py
+++ convertToHex.py
@@ -4,6 +4,8 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i','--input', dest='input', required=True, help='input dir file name')
+parser.add_argument('--col', dest='col', required=True, help='name of column to extract')
+parser.add_argument('-n', dest='n', type=int, required=True, help='number of channels')
 args = parser.parse_args()
 
 inputFile = args.input
@@ -12,8 +14,9 @@ df = pd.read_csv(inputFile,skipinitialspace=True)
 
 vHex = np.vectorize(hex)
 
-cols = [f'ePortRxDataGroup_{i}' for i in range(12)]
-
+# e.g. ePortRxDataGroup_
+cols = [f'%s_{i}'%args.col for i in range(args.n)]
+print(cols)
 df[cols] = vHex(df[cols])
 
 df.to_csv(inputFile.replace('.csv','_HEX.csv'),index=None)
